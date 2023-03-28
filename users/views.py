240c90729ddc1d3c42d7101987212add373e8306
Ru960413 <wendyhu0413@gmail.com>
2023-03-28 17:36:37 +0800

    implementation of user login and logout

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -10,12 +13,57 @@ def homePage(request):
 
 
 def UserLogin(request):
-    context= {}
-    return render(request, 'users/login.html', context)
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            pass
+
+        user=authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/dashboard')
+        else:
+            pass
+
+    return render(request, 'users/login.html')
 
 
+# This function is not working...
 def register(request):
-    # form = CustomUserCreationForm()
-    # context = {'form':form}
-    context = {}
+    form = CustomUserCreationForm()
+    
+    if request.method == "POST":
+        form=CustomUserCreationForm(request.POST)
+        # print(form)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.username=user.username.lower()
+            user.save()
+
+            login(request, user)
+
+            return redirect('/dashboard')
+
+
+
+    context = {'form':form}
     return render(request, 'users/register.html', context)
+
+
+
+@login_required(login_url='login')
+def UserLogout(request):
+
+    logout(request)
+    return render(request, "users/index.html")
+
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'users/dashboard.html')

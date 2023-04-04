19654ec1144404170e576d8ba4bd96bac3d6d5ac
Ru960413 <wendyhu0413@gmail.com>
2023-04-04 17:20:15 +0800

    add a new column "type" for transaction table

--- bank/models.py
+++ bank/models.py
@@ -8,6 +8,7 @@ class Transaction(models.Model):
     account_id = models.ForeignKey(BankAccount, on_delete=models.CASCADE, null=False, blank=False)
     time = models.DateTimeField(auto_now=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-
+    type = models.CharField(max_length=10, null=True)
+    
     def __str__(self):
-        return str(self.account_id)
+        return str(self.amount)

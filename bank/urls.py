de1ae85c9af7240356c4bed3a8a7564d236562ba
Ru960413 <wendyhu0413@gmail.com>
2023-04-06 14:27:28 +0800

    clean up code

--- bank/urls.py
+++ bank/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     # path takes 3 parameters, first: the path, second: the function which will then trigger HttpResponse
-    
-    # setting project page to the default page
     path('',  views.bankDashboard, name='bankDashboard'),
     path('deposit/',  views.deposit, name='deposit'),
     path('withdrawal/',  views.withdrawal, name='withdrawal'),

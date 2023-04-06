6a54d4df4dc72371f019f4d6be9144bd4423dee2
Ru960413 <wendyhu0413@gmail.com>
2023-04-06 17:23:18 +0800

    create currency transaction table

--- currency/models.py
+++ currency/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from users.models import BankAccount
 # Create your models here.
+
+class currencyTransaction(models.Model):
+    account_id = models.ForeignKey(BankAccount, on_delete=models.CASCADE, null=False, blank=False)
+    time = models.DateTimeField(auto_now=True)
+    currency = models.CharField(max_length=20, null=False, blank=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10, null=False)
+
+
+    def __str__(self):
+        return str(self.amount)

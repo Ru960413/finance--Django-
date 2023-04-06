2f5c3935120403c37563270bd4acbdf7ebc10e5d
Ru960413 <wendyhu0413@gmail.com>
2023-04-06 17:22:28 +0800

    add a blank line

--- bank/models.py
+++ bank/models.py
@@ -10,5 +10,6 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     type = models.CharField(max_length=10, null=False)
     
+    
     def __str__(self):
         return str(self.amount)

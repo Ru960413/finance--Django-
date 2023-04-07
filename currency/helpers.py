25b2a93f2452eceda35132307b403c9973c8fcfb
Ru960413 <wendyhu0413@gmail.com>
2023-04-07 17:18:11 +0800

    modify lookup function

--- currency/helpers.py
+++ currency/helpers.py
@@ -3,6 +3,7 @@ import requests
 import urllib.parse
 import math
 
+
 # logic: how much TWD do user need to get the amount of currency they need?
 def convert(currency, amount):
     try:
@@ -24,11 +25,13 @@ def convert(currency, amount):
     except (KeyError, ValueError, TypeError):
         return None
 
+
+
 # for user to query currency rate
-def lookup(currency):
+def lookup(from_currency, to_currency):
     try:
         api_key = 'de854dd1ac96a4c45b041347'
-        url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{urllib.parse.quote_plus(currency)}/TWD/1'
+        url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{urllib.parse.quote_plus(from_currency)}/{urllib.parse.quote_plus(to_currency)}/1'
         response = requests.get(url)
         response.raise_for_status()    
     except requests.RequestException:

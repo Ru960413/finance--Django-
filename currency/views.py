8aae90f3ea8bc0cbff149355d79e59673efec479
Ru960413 <wendyhu0413@gmail.com>
2023-04-07 16:02:07 +0800

    import function from helpers

--- currency/views.py
+++ currency/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from users.models import BankAccount
-import requests
+from .helpers import convert, twd, lookup
 # Create your views here.
 
 
@@ -10,18 +10,6 @@ def currencyDashboard(request):
     page="currency"
     context={"page":page}
     return render(request, 'currency/currencyDashboard.html', context)
-
-
-# THis is not working... 
-def api(request):
-    # interact with exchange rate currency api
-
-    url = 'https://v6.exchangerate-api.com/v6/de854dd1ac96a4c45b041347/latest/TWD'
-
-    response = requests.get(url)
-    data = response.json()
-
-    return render(request, 'currency/currencyDashboard.html', {"data": data["conversion_rates"]["HKD"]})
 
 
 

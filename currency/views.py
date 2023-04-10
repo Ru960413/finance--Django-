a5c38935e6f6c300ae94548f7ddcb2ffb4e49f95
Ru960413 <wendyhu0413@gmail.com>
2023-04-10 17:24:44 +0800

    finish quote feature and add new comments

--- currency/views.py
+++ currency/views.py
@@ -14,13 +14,34 @@ def currencyDashboard(request):
 
 
 @login_required(login_url='login')
-def buy(request):
-    pass
+def quote(request):
+    if request.method=="POST":
+        action="quote"
+        from_currency=request.POST["from_currency"]
+        amount=request.POST["amount"]
+
+        result=lookup(from_currency, amount)
+        if from_currency==None:
+            message="Please enter the currency code you want to quote"
+            context={"action": action, "message": message}
+            return render(request, "currency/fail.html", context)
+        
+        elif result==None:
+            message="The currency that you are looking for doesn't exist"
+            context={"action": action, "message": message}
+            return render(request, "currency/fail.html", context)
+        
+        else:
+            
+            context={"amount":amount, "result": twd(result), "from_currency":from_currency}
+            return render(request, "currency/quoted.html", context)
+        
+    return render(request, 'currency/currencyDashboard.html') 
 
 
 
 @login_required(login_url='login')
-def quote(request):
+def buy(request):
     pass
 
 
@@ -31,6 +52,14 @@ def sell(request):
 
 
 
+# access currencyTransaction table and then load the data dynamically to currencyDashboard
 @login_required(login_url='login')
 def history(request):
+    pass
+
+
+
+# access currencyTransaction table and then load the data dynamically to currencyDashboard
+@login_required(login_url='login')
+def currencyOwn(request):
     pass

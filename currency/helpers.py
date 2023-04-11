a1753c90ae74ae2aa8a6a733d97d70ccf3567d30
Ru960413 <wendyhu0413@gmail.com>
2023-04-11 17:11:37 +0800

    commented "commented"

--- currency/helpers.py
+++ currency/helpers.py
@@ -5,25 +5,25 @@ import math
 
 
 # logic: how much TWD do user need to get the amount of currency they need?
-def convert(currency, amount):
-    try:
-        api_key = 'de854dd1ac96a4c45b041347'
-        url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{urllib.parse.quote_plus(currency)}/TWD/{amount}'
-        response = requests.get(url)
-        response.raise_for_status()    
-    except requests.RequestException:
-        return None
+# def convert(currency_code, amount):
+#     try:
+#         api_key = 'de854dd1ac96a4c45b041347'
+#         url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{urllib.parse.quote_plus(currency_code)}/TWD/{amount}'
+#         response = requests.get(url)
+#         response.raise_for_status()    
+#     except requests.RequestException:
+#         return None
 
-    try:
-        quote = response.json()
-        return {
-            "currency":quote["base_code"],
-            "rate": quote["conversion_rate"],
-            "result": quote["conversion_result"],
+#     try:
+#         quote = response.json()
+#         return {
+#             "currency":quote["base_code"],
+#             "rate": quote["conversion_rate"],
+#             "result": quote["conversion_result"],
 
-        }
-    except (KeyError, ValueError, TypeError):
-        return None
+#         }
+#     except (KeyError, ValueError, TypeError):
+#         return None
 
 
 
@@ -43,6 +43,24 @@ def lookup(from_currency, amount):
 
     except (KeyError, ValueError, TypeError):
         return None
+    
+
+
+# def convertBalance(to_currency, balance):
+#     try:
+#         api_key = 'de854dd1ac96a4c45b041347'
+#         url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/TWD/{urllib.parse.quote_plus(to_currency)}/{balance}'
+#         response = requests.get(url)
+#         response.raise_for_status()    
+#     except requests.RequestException:
+#         return None
+
+#     try:
+#         quote = response.json()
+#         return quote["conversion_result"]
+
+#     except (KeyError, ValueError, TypeError):
+#         return None
 
 
 def twd(value):

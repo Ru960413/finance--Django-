a79bb704c631f3968ede882160a3eba4fb5780f5
Ru960413 <wendyhu0413@gmail.com>
2023-04-14 21:22:27 +0800

    connect db with user model

--- currency/models.py
+++ currency/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from users.models import BankAccount
+from django.contrib.auth.models import User
 # Create your models here.
 
 class currencyTransaction(models.Model):
     account_id = models.ForeignKey(BankAccount, on_delete=models.CASCADE, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
     time = models.DateTimeField(auto_now=True)
     currency = models.CharField(max_length=20, null=False, blank=False)
     amount = models.DecimalField(max_digits=10, decimal_places=2)

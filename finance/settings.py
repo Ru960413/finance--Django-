b0591d4a543906ce01e1d3203043cda2d34aade9
Ru960413 <wendyhu0413@gmail.com>
2023-04-17 12:48:48 +0800

    connect to sqlite db

--- finance/settings.py
+++ finance/settings.py
@@ -86,16 +86,16 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'finance',
-        'USER': 'postgres',
-        'PASSWORD': 'qaz12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'finance',
+#         'USER': 'postgres',
+#         'PASSWORD': 'qaz12345',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 # run "pip install psycopg2" in the terminal to connect the postgreSQL db to app
 
 # Password validation

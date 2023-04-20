bb3e7e81afcdb75f035e90088b0db2e367a03af2
Ru960413 <wendyhu0413@gmail.com>
2023-04-20 16:48:28 +0800

    add email config

--- finance/settings.py
+++ finance/settings.py
@@ -128,6 +128,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+# allow Django to send email to user(using TLS port)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'huiru.test@gmail.com'
+EMAIL_HOST_PASSWORD= 'pguutozikjbimytk'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/

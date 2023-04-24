f5774e135bc2b1c777c315847b2fd0541bd49d95
Ru960413 <wendyhu0413@gmail.com>
2023-04-24 12:52:36 +0800

    clean up code

--- currency/helpers.py
+++ currency/helpers.py
@@ -43,24 +43,6 @@ def lookup(from_currency, amount):
 
     except (KeyError, ValueError, TypeError):
         return None
-    
-
-
-# def convertBalance(to_currency, balance):
-#     try:
-#         api_key = 'de854dd1ac96a4c45b041347'
-#         url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/TWD/{urllib.parse.quote_plus(to_currency)}/{balance}'
-#         response = requests.get(url)
-#         response.raise_for_status()    
-#     except requests.RequestException:
-#         return None
-
-#     try:
-#         quote = response.json()
-#         return quote["conversion_result"]
-
-#     except (KeyError, ValueError, TypeError):
-#         return None
 
 
 def twd(value):

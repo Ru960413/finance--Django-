db957fa1f39772b38d52240343af9164b38c58f6
Ru960413 <wendyhu0413@gmail.com>
2023-04-28 17:07:32 +0800

    updated

--- finance/settings.py
+++ finance/settings.py
@@ -30,8 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'finance-django-production.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.localhost', 'finance-django-production.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS=[
+    'https://finance-django-production.up.railway.app'
+]
 
 # Application definition
 
